297e6de50f0843b7c4a6d64922640fb9a53b343e
tarus <tarus@Ubuntu.myguest.virtualbox.org>
2023-03-16 11:41:39 +0530

    initial commit

--- build.sh
+++ build.sh
@@ -12,12 +12,13 @@ sudo docker build -f reciever/Dockerfile .
 
 # Docker command to start the rabbit message queue server
 
-sudo docker run -it --rm --name rabbitmq -p 5672:5672 -p 8080:15672 rabbitmq:3.11-management
+# sudo docker run -it --rm --name rabbitmq -p 5672:5672 -p 8080:15672 rabbitmq:3.11-management
 
 # Start the publisher/sender to push the messages to the rabbitmq server
 
-go run sender/publisher.go
+cd sender && go run publisher.go
 
+cd ../
 # Start the consumer/reciever to pull the message to the rabbitmq server
 
-go run reciever/consumer.go
+# cd reciever && go run reciever/consumer.go
